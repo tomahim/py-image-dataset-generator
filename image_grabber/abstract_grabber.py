e5031e7229333b8355b149f9402597179a6b488d
tomahim <thimblot@gmail.com>
2018-02-13 23:28:31 +0100

    refactor: add type hint

--- image_grabber/abstract_grabber.py
+++ image_grabber/abstract_grabber.py
@@ -1,15 +1,17 @@
 from abc import ABCMeta, abstractmethod
+from .grabbed_image import GrabbedImage
+from typing import List
 
 
 class AbstractGrabber:
     __metaclass__ = ABCMeta
 
     @property
-    def full_image(self):
-        """ boolean representing if downloaded image is full size or thumbnail """
+    def full_image(self) -> bool:
+        """ If true grab the full image size, if false the thumbnail (if available) """
         raise NotImplementedError
 
     @abstractmethod
-    def get_images_url(self, keyword):
-        """ it should return an array of GrabbedImage objects with at least base64 or url defined """
+    def get_images_url(self, keyword: str) -> List[GrabbedImage]:
+        """ it should return extracted data with at least base64 or url defined """
         pass
